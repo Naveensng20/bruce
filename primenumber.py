77c9a8371a36fa0f2c3f635b370512034c7b086b
Naveensng20 <46569882+Naveensng20@users.noreply.github.com>
2019-02-16 13:38:41 +0530

    Update primenumber.py

--- primenumber.py
+++ primenumber.py
@@ -1,7 +1,7 @@
-num=int(raw_input())
-if num > 1:
-   for i in range(2,num):
-       if (num % i) == 0:
+NUM=int(raw_input())
+if NUM > 1:
+   for i in range(2,NUM):
+       if (NUM % i) == 0:
            print("no")
            break
    else:
