3c355a69525019952038fa1dd0eb415417099e91
Naveensng20 <46569882+Naveensng20@users.noreply.github.com>
2019-02-17 19:38:10 +0530

    Create armstrongbwinterval.py

---
+++ armstrongbwinterval.py
@@ -0,0 +1,16 @@
+a=input()
+nk=a.split()
+num1=int(nk[0])
+num2=int(nk[1]) 
+for num in range(num1,num2):
+	pow=len(str(num))
+	sum=0
+	temp=num
+	while(temp>0):
+		sum=sum+((temp%10)**pow)
+		temp=temp//10
+	if(sum==num):
+		print(num),
+		
+	
+
