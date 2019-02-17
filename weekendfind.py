b551c5a415a6c0597b37698a84c6791ff98ffda7
Naveensng20 <46569882+Naveensng20@users.noreply.github.com>
2019-02-17 20:13:25 +0530

    Create weekendfind.py

---
+++ weekendfind.py
@@ -0,0 +1,8 @@
+day=input()
+if(day==("Monday" or "Tuesday" or "Wednesday" or "Thursdday" or "friday")):
+	print("no")
+elif(day==("Saturday" or "saturday") or ("Sunday" or "sunday")):
+	print("yes")
+else:
+	print("enter the day")
+	
